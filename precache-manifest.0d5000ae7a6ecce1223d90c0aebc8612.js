f96596abeaebca0c10734aaf42c9619b8aa3cab5
roshansingha <roshansinghaagt@gmail.com>
2020-06-18 19:39:22 +0530

    Updates

---
+++ precache-manifest.0d5000ae7a6ecce1223d90c0aebc8612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f3b52ad3650d9ae632adb7dc07448e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "529c1ac3fb3181266c29",
+    "url": "/static/css/main.e4ee1478.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529c1ac3fb3181266c29",
+    "url": "/static/js/main.1b199a79.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
